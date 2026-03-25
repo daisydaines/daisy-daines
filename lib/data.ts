084bb398c28d53deab33d75c225bd61c9301df6c
daisydaines <daisy@daines.net>
2026-03-25 10:38:30 -0600

    workouts

--- lib/data.ts
+++ lib/data.ts
@@ -173,7 +173,7 @@ export const liftsMetadata = {
 };
 
 export const lifts: Lift[] = [
-  { label: "bench", value: 230, previous: 225, unit: "lbs" },
+  { label: "bench", value: 240, previous: 225, unit: "lbs" },
   { label: "squat", value: 325, previous: 315, unit: "lbs" },
   { label: "deadlift", value: 350, previous: 335, unit: "lbs" },
   { label: "pullups", value: 11, previous: 10, unit: "reps" },
